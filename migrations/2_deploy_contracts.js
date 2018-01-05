870aae3b4e5897d1ab4da9371d5ae4685d5b5751
Luis Philipe Fidelis <luizsale10@gmail.com>
2018-01-05 14:31:35 -0200

    print deploy's owner

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,14 @@
 const Faucet = artifacts.require('./Faucet.sol');
 const BigNumber = web3.BigNumber;
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   const rate = new BigNumber(300);
   deployer.deploy(Faucet, rate).then((deployed) => {
     Faucet.deployed().then(deployed => {
       deployed.token.call().then(token => {
         console.log(`Faucet: ${Faucet.address}`)
         console.log(`Token: ${token}`)
+        console.log(`Deploy Owner: ${accounts[0]}`)
       });
     });  
   });
